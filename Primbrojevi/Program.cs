ee44f8331392a67023de1b4d35b9717a8f6da14f
vitoth <viktor965@gmail.com>
2016-11-02 20:46:23 +0100

    Refactoring

--- Primbrojevi/Program.cs
+++ Primbrojevi/Program.cs
@@ -29,54 +29,83 @@ namespace Vsite.Pood
             }
         }
 
+        static bool[] f;
+
         // Primjer iz knjige  Robert C. Martin: "Agile Software Development"!!!
-        public static int[] GenerirajPrimBrojeve(int max) 
+        public static int[] GenerirajPrimBrojeve(int max)
         {
-            if (max >= 2)
+            return IzdvojiPrimBrojeve(max);
+        }
+
+        private static int[] IzdvojiPrimBrojeve(int max)
+        {
+            if (max < 2)
+                return new int[0]; // vrati prazan niz
+            else
             {
                 // deklaracije
-                int s = max + 1; // duljina niza
-                bool[] f = new bool[s]; // niz s primbrojevima
-                int i;
-
-                // inicijaliziramo sve na true
-                for (i = 0; i < s; ++i)
-                    f[i] = true;
-
-                // ukloni 0 i 1 koji su primbrojevi po definiciji
-                f[0] = f[1] = false;
+                InicijalizirajSito(max);
 
                 // sito (ide do kvadratnog korijena maksimalnog broja)
-                int j;
-                for (i = 2; i < Math.Sqrt(s); ++i)
-                {
-                    if (f[i]) // ako je i prekrižen, prekriži i višekratnike
-                    {
-                        for (j = 2 * i; j < s; j += i)
-                            f[j] = false; // višekratnik nije primbroj
-                    }
-                }
+                Prosijaj();
 
-                // koliko je primbrojeva?
-                int broj = 0;
-                for (i = 0; i < s; ++i)
-                {
-                    if (f[i])
-                        ++broj;
-                }
 
-                int[] primovi = new int[broj];
+                //da bi rezervirali duljinu niza
+                int[] primovi = new int[IzračunajKolikoJePrimBrojeva()];
 
                 // prebaci primbrojeve u rezultat
-                for (i = 0, j = 0; i < s; ++i)
+                for (int i = 2, j = 0; i < f.Length; ++i)
                 {
                     if (f[i])
                         primovi[j++] = i;
                 }
                 return primovi; // vrati niz brojeva
             }
-            else
-                return new int[0]; // vrati prazan niz
+
+        }
+
+        private static int IzračunajKolikoJePrimBrojeva()
+        {
+            int broj = 0;
+            for (int i = 2; i < f.Length; ++i)
+            {
+                if (f[i])
+                    ++broj;
+            }
+
+            return broj;
+        }
+
+        private static void Prosijaj()
+        {
+
+            for (int i = 2; i < DajNajvećiPrimFaktor(); ++i)
+            {
+                if (f[i]) // ako je i prekrižen, prekriži i višekratnike
+                    EliminirajVišekratnike(i);
+            }
+        }
+
+        private static void EliminirajVišekratnike(int i)
+        {
+            for (int j = 2 * i; j < f.Length; j += i)
+                f[j] = false; // višekratnik nije primbroj
+        }
+
+        private static int DajNajvećiPrimFaktor()
+        {
+            return (int)Math.Sqrt(f.Length);
+        }
+
+        private static void InicijalizirajSito(int max)
+        {
+            f = new bool[max+1]; // niz f.Length primbrojevima
+            int i;
+
+            // inicijaliziramo sve na true kao da su svi prim brojevi pretpostavimo
+            for (i = 2; i < f.Length; ++i)
+                f[i] = true;
+
         }
     }
 }
---
+++ Testovi/TestPrimBrojeva.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Pood;
+
+namespace Testovi
+{
+    [TestClass]
+    public class TestPrimBrojeva
+    {
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaPrazanNizZa0()
+        {
+            Assert.AreEqual(0, Program.GenerirajPrimBrojeve(0).Length);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaPrazanNizZa1()
+        {
+            Assert.AreEqual(0, Program.GenerirajPrimBrojeve(1).Length);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaNizKojiSadržiSamo2ZaArgument2()
+        {
+            Assert.AreEqual(1, Program.GenerirajPrimBrojeve(2).Length);
+            Assert.AreEqual(2, Program.GenerirajPrimBrojeve(2)[0]);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaNizKojiSadrži25PrimBrojevaZaArgument100()
+        {
+            var primBrojevi = Program.GenerirajPrimBrojeve(100);
+            Assert.AreEqual(25, primBrojevi.Length);
+            Assert.AreEqual(2, primBrojevi[0]);
+            Assert.AreEqual(97, primBrojevi[primBrojevi.Length-1]);
+        }
+    }
+}
